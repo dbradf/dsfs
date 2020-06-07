e01405dd87141923a5740642d93d52e139652046
David Bradford <david.bradford@mongodb.com>
2020-06-07 11:53:30 -0400

    chapter 6: probablity

---
+++ src/dsfs/stats/probability.py
@@ -0,0 +1,72 @@
+from collections import Counter
+import math
+import random
+
+import matplotlib.pyplot as plt
+
+SQRT_TWO_PI = math.sqrt(2 * math.pi)
+
+
+def uniform_pdf(x: float) -> float:
+    return 1 if 0 <= x < 1 else 0
+
+
+def uniform_cdf(x: float) -> float:
+    if x < 0:
+        return 0
+    elif x < 1:
+        return x
+    else:
+        return 1
+
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (SQRT_TWO_PI * sigma))
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+
+def inverse_normal_cdf(p: float, mu: float = 0, sigma: float = 1, tolerance: float = 0.00001) -> float:
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+
+    low_z = -10.0
+    hi_z = 10.0
+    while hi_z - low_z > tolerance:
+        mid_z = (low_z + hi_z) / 2
+        mid_p = normal_cdf(mid_z)
+        if mid_p < p:
+            low_z = mid_z
+        else:
+            hi_z = mid_z
+        
+    return mid_z
+
+
+def bernoulli_trail(p: float) ->  int:
+    return 1 if random.random() < p else 0
+
+
+def binomial(n: int, p: float) -> int:
+    return sum(bernoulli_trail(p) for _ in range(n))
+
+
+def binomial_histogram(p: float, n: int, num_points: int) -> None:
+    data = [binomial(n, p) for _ in range(num_points)]
+
+    histogram = Counter(data)
+    plt.bar([x - 0.4 for x in histogram.keys()],
+            [v / num_points for v in histogram.values()],
+            0.8, color='0.75')
+
+    mu = p * n
+    sigma = math.sqrt(n * p * (1 -p))
+
+    xs = range(min(data), max(data) + 1)
+    ys = [normal_cdf(i + 0.5, mu, sigma) - normal_cdf(i - 0.5, mu, sigma)
+          for i in xs]
+    plt.plot(xs, ys)
+    plt.title("Binomial Distribution vs. Normal Approximation")
+    plt.show()
